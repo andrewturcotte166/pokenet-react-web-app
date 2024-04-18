5af4706a3f4f39f6fb2c85606e6e105912d2d1bd
Ariel Shtivelberg <48467213+ashtivelberg@users.noreply.github.com>
2024-04-18 16:17:48 -0400

    nav bar logged in logic + search results listing (#2)
    * varied navbar/homescreen for logged in users
    
    * search lists 50 results
    
    * clean up home welcome message logic
    
    * style fix
    
    ---------
    
    Co-authored-by: andrewturcotte166 <turcotte.an@northeastern.edu>

--- src/pokenet/Home/index.tsx
+++ src/pokenet/Home/index.tsx
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react";
+import * as client from "../Users/client";
+
 function Home() {
+    const [profile, setProfile] = useState<any>();
+    const fetchProfile = async () => {
+        try {
+            const account = await client.profile();
+            setProfile(account);
+        } catch (error) {
+            console.log("Not logged in")
+        }
+    };
+
+    useEffect(() => {
+        fetchProfile();
+    }, []); 
+    
     return(
         <div className="p-4">
             <h1>Welcome to Pokenet!</h1>
+            {profile && <h3>Welcome, {profile.role} {profile.firstName}</h3>}
         </div>
     );
 }
