06faa720a59c79369a0eacdeda362750f0682b3c
Alec Varma <alec_varma@yahoo.com>
2024-04-24 19:27:14 -0400

    pkmn spotlight + small description of site

--- src/pokenet/Home/index.tsx
+++ src/pokenet/Home/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import * as client from "../Users/client";
 import HomeView from "./HomeView";
+import { TbPokeball } from "react-icons/tb";
 
 function Home() {
     const [profile, setProfile] = useState<any>();
@@ -19,7 +20,13 @@ function Home() {
 
     return(
         <div className="p-4">
-            <h1>Welcome to Pokenet!</h1>
+            <h1 className="display-4">Welcome to Pokenet!</h1>
+            <p className="lead">This is a social network for Pokemon fans where you can:</p>
+            <ul>
+                <li>Build your perfect team!</li>
+                <li>Connect with Friends and Professors!</li>
+                <li>Find more information about your favorite Pokemon!</li>
+            </ul>
             {!profile && <HomeView/>}
             {profile && <h3>Welcome, {profile.role.charAt(0) + profile.role.slice(1).toLowerCase()} {profile.firstName.charAt(0).toUpperCase() + profile.firstName.slice(1).toLowerCase()}</h3>}
         </div>
